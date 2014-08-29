bdf1737401107d4974826763eafb327d46e0049e
huangjp <gbchuangjinping@sina.com>
2014-08-29 18:03:28 +0800

    首次提交代码

---
+++ myFlow/src/main/java/org/myFlow/util/MyUtil.java
@@ -0,0 +1,17 @@
+/*
+ * 文件名：MyUtil.java	 
+ * 时     间：下午6:00:32
+ * 作     者：huangjp       
+ * 版     权： 2014-2022  智慧园区, 公司保留所有权利.
+ * 联     系：www.szyungu.com
+ */
+package org.myFlow.util;
+/** 
+ * @ClassName: MyUtil 
+ * @Description: (这里用一句话描述这个类的作用) 
+ * @author huangjp
+ * @date 2014年8月29日 下午6:00:32 
+ */
+public class MyUtil {
+
+}
